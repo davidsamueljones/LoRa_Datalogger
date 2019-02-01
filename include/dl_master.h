0491493ab97d85ab1f0bcb9bfa89cb73623a9bc8
David Jones <davidsamueljones@icloud.com>
2019-02-01 14:55:04 +0000

    Added explicit guards to stop multiple setup and loop declarations
    Reverted source excludes as they caused compilations to fail.
    
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- include/dl_master.h
+++ include/dl_master.h
@@ -1,8 +1,10 @@
 #ifndef DL_MASTER_H
 #define DL_MASTER_H
 
+#ifdef DL_MASTER
 void setup();
 
 void loop();
+#endif
 
 #endif // DL_MASTER_H
--- include/dl_slave.h
+++ include/dl_slave.h
@@ -1,8 +1,10 @@
 #ifndef DL_SLAVE_H
 #define DL_SLAVE_H
 
+#ifdef DL_SLAVE
 void setup();
 
 void loop();
+#endif
 
 #endif // DL_SLAVE_H
