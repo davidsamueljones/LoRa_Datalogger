4567cb78b6fbf4cae3391ed25d737e15313d4f82
David Jones <davidsamueljones@icloud.com>
2019-02-09 00:00:04 +0000

    Moved BOARD_ID declarations to header files
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- include/dl_master.h
+++ include/dl_master.h
@@ -2,6 +2,14 @@
 #define DL_MASTER_H
 
 #ifdef DL_MASTER
+
+// Pass an alternative during compilation if multiple of each board type
+#ifndef ARG_BOARD_ID
+#define ARG_BOARD_ID (__COUNTER__)
+#endif
+// Top bit not set for Masters
+#define BOARD_ID (uint8_t) (ARG_BOARD_ID & 0x7F)
+
 void setup();
 
 void loop();
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,10 @@
 #error Cannot compile as both DL_MASTER and DL_SLAVE
 #endif
 
-// Pass an alternative during compilation if multiple of each board type
-// TODO: Could use __COUNTER__ macro to do automatically
-#ifndef ARG_BOARD_ID
-#define ARG_BOARD_ID (1)
-#endif
-
 // Include appropriate 
 #ifdef DL_MASTER
 #include "dl_master.h"
-// Top bit not set for Masters
-#define BOARD_ID (uint8_t) (ARG_BOARD_ID & 0x7F)
 #endif
 #ifdef DL_SLAVE
 #include "dl_slave.h"
-// Top bit set for Slaves
-#define BOARD_ID (0x80 + (uint8_t) (ARG_BOARD_ID & 0x7F))
 #endif
