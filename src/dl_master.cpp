f9c07143dfde46506ca6b1315eaee6225cb165c8
David Jones <davidsamueljones@icloud.com>
2019-02-10 15:38:17 +0000

    Shifted control loop to use intterupt behaviour for switches
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

--- src/dl_master.cpp
+++ src/dl_master.cpp
@@ -1,23 +1,33 @@
 #ifdef DL_MASTER
 
 #include <Arduino.h>
+#include <TimeLib.h>
+
+#include "dl_master.h"
 #include "breakout.h"
 #include "radio.h"
 
+
+static void ISR_switch_state_change(void);
 static void run_test_defs(void);
 static void handle_loop_mode_mid(void);
 static void handle_loop_mode_bot(void);
 
 lora_cfg_t g_testdef_cfg_a = {
-	.freq = 868.0,
-	.sf = 7,
-	.tx_dbm = 14,
-	.bw = 125000,
-	.cr4_denom = 5,
-	.crc = true,
+  .freq = 868.0,
+  .sf = 7,
+  .tx_dbm = 14,
+  .bw = 125000,
+  .cr4_denom = 5,
+  .crc = true,
 };
 
 lora_testdef_t g_testdef_a;
+
+void print_date_time() {
+  Serial.printf("[%04d-%02d-%02d] %02d:%02d:%02d\n", 
+      year(), month(), day(), hour(), minute(), second());
+}
 
 void setup() {
   // Initialise breakout board
@@ -27,22 +37,42 @@ void setup() {
   breakout_set_led(BO_LED_3, true); // Waiting for rest of boot
 
   // Initialise serial connectivity
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(100);
   // Warning: Not valid for all possible microcontrollers, ideal behaviour will
   // mean bootup will be delayed until serial monitor open
   while (breakout_get_switch_state() == sw_state_bot && !Serial) {}
   breakout_set_led(BO_LED_1, false);
 
-  Serial.printf("Booting...\n");
+  Serial.printf("Booting as Master...\n");
   
+  // RTC configuration occurs during breakout board initialisation
+  Serial.printf("RTC sync %s!\n", timeStatus() == timeSet ? "successful" : "failed");
+  Serial.printf("Current [Date] Time: ");
+  print_date_time();
+
   // Initialise radio, keep track using global instance
-  lora_module_t lora_module = {RFM95_CS, RFM95_RST, RFM95_INT};
+  lora_module_t lora_module = {BOARD_ID, RFM95_CS, RFM95_RST, RFM95_INT};
   g_radio_a = new LoRaModule(&lora_module, &hc_base_cfg);
-  if (!g_radio_a->radio_init()) {
+  bool radio_init_sucess = g_radio_a->radio_init();
+  Serial.printf("Radio initialisation %s!\n", radio_init_sucess ? "successful" : "failed");
+  Serial.printf("* Radio ID: 0x%02X\n", lora_module.radio_id);
+  if (!radio_init_sucess) {
     return;
   }
   breakout_set_led(BO_LED_2, false);
+
+  // init_sd_card();
+  breakout_set_led(BO_LED_3, false);
+
+  Serial.printf("Configuring switch, ensure it is in its middle position...\n");
+  // Wait for switch to return to middle position
+  while (breakout_get_switch_state() != sw_state_mid) {}
+  // Attach switch interrupts 
+  attachInterrupt(digitalPinToInterrupt(BO_SWITCH_PIN1), ISR_switch_state_change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(BO_SWITCH_PIN2), ISR_switch_state_change, CHANGE);
+  Serial.printf("Switch configured!\n");
+
 
   // Define an inbuilt test definition
   strcpy(g_testdef_a.id, "Test_A");
@@ -50,16 +80,11 @@ void setup() {
   g_testdef_a.packet_len = 60;
   g_testdef_a.cfg = g_testdef_cfg_a;
 
-	// init_sd_card();
-  Serial.printf("Booted\n");
-  breakout_set_led(BO_LED_3, false);
-  // Wait for switch to return to middle position
-  while (breakout_get_switch_state() != sw_state_mid) {}
+  Serial.printf("Booted as Master!\n");
+
 }
 
 void loop() {
-  // Poll switch to choose behaviour, keep internal function behaviour
-  // short so it can react to switch changes
   switch (breakout_get_switch_state()) {
     case sw_state_top:
       run_test_defs();
@@ -70,12 +95,20 @@ void loop() {
       break;
     case sw_state_bot:
       handle_loop_mode_bot();
+      delay(100);
       break;
     default:
       break;
   }
 }
 
+/**
+ * Set any relevent flags to indicate that the current functions should be
+ * exited to allow for a change in device behaviour. 
+ */
+static void ISR_switch_state_change(void) {
+  g_radio_a->set_interrupt(true);
+}
 
 static void run_test_defs(void) {
   Serial.printf("Running test defs...\n");
@@ -84,15 +117,14 @@ static void run_test_defs(void) {
   breakout_set_led(BO_LED_2, false);
   breakout_set_led(BO_LED_3, true);
 
+  // Clear any set interrupt
+  g_radio_a->set_interrupt(false);
   // Reset to agreed base 
   g_radio_a->reset_to_base_cfg();
 
   // Handshake to pass over test def
   g_radio_a->send_testdef(&g_testdef_a);
-  // Wait for packets
-  while (true) {
-
-  }
+  while (breakout_get_switch_state() != sw_state_mid) {}
 }
 
 static void handle_loop_mode_mid(void) {
@@ -100,16 +132,14 @@ static void handle_loop_mode_mid(void) {
   breakout_set_led(BO_LED_1, false);
   breakout_set_led(BO_LED_2, true);
   breakout_set_led(BO_LED_3, false);
+  delay(100);
 }
 
-/**
- * Listen for test packets on the base configuration and record any received.
- */
 static void handle_loop_mode_bot(void) {
-  // Simple Listen 
   breakout_set_led(BO_LED_1, true);
   breakout_set_led(BO_LED_2, false);
   breakout_set_led(BO_LED_3, false);
+  delay(100);
 }
 
 #endif
--- src/dl_slave.cpp
+++ src/dl_slave.cpp
@@ -1,31 +1,59 @@
 #ifdef DL_SLAVE
 
+#include <Arduino.h>
+
+#include "dl_slave.h"
 #include "breakout.h"
 #include "radio.h"
 
+static void ISR_switch_state_change(void);
 static void run_test_defs(void);
 static void handle_loop_mode_mid(void);
 static void handle_loop_mode_bot(void);
 
 void setup() {
-  Serial.begin(9600);
-  delay(100);
-  Serial.printf("Booting...");
-  
   // Initialise breakout board
   breakout_init();
-  // Initialise radio, keep track using global instance
-  lora_module_t lora_module = {RFM95_CS, RFM95_RST, RFM95_INT};
-  g_radio_a = new LoRaModule(&lora_module, &hc_base_cfg);
-  g_radio_a->radio_init(); // TODO: Handle failure
+  breakout_set_led(BO_LED_1, true); // Waiting for serial
+  breakout_set_led(BO_LED_2, true); // Waiting for radio
+  breakout_set_led(BO_LED_3, true); // Waiting for rest of boot
+
+  // Initialise serial connectivity
+  Serial.begin(9600);
+  delay(100);
+  // Warning: Not valid for all possible microcontrollers, ideal behaviour will
+  // mean bootup will be delayed until serial monitor open
+  while (breakout_get_switch_state() == sw_state_bot && !Serial) {}
+  breakout_set_led(BO_LED_1, false);
+
+  Serial.printf("Booting as Slave...\n");
   
-	// init_sd_card();
-  Serial.printf("Booted");
+  // Initialise radio, keep track using global instance
+  lora_module_t lora_module = {BOARD_ID, RFM95_CS, RFM95_RST, RFM95_INT};
+  g_radio_a = new LoRaModule(&lora_module, &hc_base_cfg);
+  bool radio_init_sucess = g_radio_a->radio_init();
+  Serial.printf("Radio initialisation %s!\n", radio_init_sucess ? "successful" : "failed");
+  Serial.printf("* Radio ID: 0x%02X\n", lora_module.radio_id);
+  if (!radio_init_sucess) {
+    return;
+  }
+  breakout_set_led(BO_LED_2, false);
+
+  // init_sd_card();
+  Serial.printf("Booted as Slave!\n");
+  breakout_set_led(BO_LED_3, false);
+
+  Serial.printf("Configuring switch, ensure it is in its middle position...\n");
+  // Wait for switch to return to middle position
+  while (breakout_get_switch_state() != sw_state_mid) {}
+  // Attach switch interrupts 
+  attachInterrupt(digitalPinToInterrupt(BO_SWITCH_PIN1), ISR_switch_state_change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(BO_SWITCH_PIN2), ISR_switch_state_change, CHANGE);
+  Serial.printf("Switch configured!\n");
+  Serial.printf("Entering control loop...\n");
 }
 
 void loop() {
-  // Poll switch to choose behaviour, keep internal function behaviour
-  // short so it can react to switch changes
   switch (breakout_get_switch_state()) {
     case sw_state_top:
       run_test_defs();
@@ -36,13 +64,20 @@ void loop() {
       break;
     case sw_state_bot:
       handle_loop_mode_bot();
+      delay(100);
       break;
     default:
       break;
   }
-
 }
 
+/**
+ * Set any relevent flags to indicate that the current functions should be
+ * exited to allow for a change in device behaviour. 
+ */
+static void ISR_switch_state_change(void) {
+  g_radio_a->set_interrupt(true);
+}
 
 static void run_test_defs(void) {
   lora_testdef_t testdef;
@@ -51,14 +86,12 @@ static void run_test_defs(void) {
   breakout_set_led(BO_LED_2, false);
   breakout_set_led(BO_LED_3, true);
 
+  // Clear any set interrupt
+  g_radio_a->set_interrupt(false);
   // Reset to agreed base 
   g_radio_a->reset_to_base_cfg();
   // Handshake to pass over test def
   g_radio_a->recv_testdef(&testdef);
-  // Wait for packets
-  while (true) {
-
-  }
 }
 
 static void handle_loop_mode_mid(void) {
